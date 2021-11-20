c1d1f972db6e37f0042e838048d78a975a735b30
Antonio David Villegas Yeguas <advy99@gmail.com>
2021-11-20 16:32:39 +0100

    devuelvo el modelo entrenado y documentación de la función

--- Trabajo_python/proyecto.py
+++ Trabajo_python/proyecto.py
@@ -21,12 +21,35 @@ import sklearn.ensemble
 import sklearn.svm
 import sklearn.neural_network
 
+### para reducción de dimensionalidad
+import sklearn.decomposition
+
 #
 # Funciones
 #
 
-def entrenar_modelo(modelo, predictores, etiquetas, predictores_test = None, etiquetas_test = None, normalizar = True, porcentaje_test = 0.2):
 
+def entrenar_modelo(modelo, predictores, etiquetas, predictores_test = None, etiquetas_test = None, normalizar = True, porcentaje_test = 0.2):
+	"""
+	Funcion para entrenar un modelo.
+	Parametros:
+		modelo: modelo a entrenar
+		predictores: Predictores a los que ajustar el modelo
+		etiquetas: Etiquetas asociadas a los predictores
+		predictores_test: Predictores para el conjunto de test, por defecto None
+		etiquetas_test: Etiquetas asociadas al conjunto de test, por defecto None
+		normalizar: Booleano para marcar si es necesario normalizar o no los predictores, por defecto True
+		porcentaje_test: Porcentaje de datos que conformarán el conjunto de test si no se ha pasado el conjunto de test
+
+	Devuelve:
+		modelo: Modelo ajustado a los predictores
+		train_accuraccy: Precisión obtenida en el conjunto de train
+		test_accuraccy: Precisión obtenida en el conjunto de test
+
+	En caso de no indicar conjuntos de test, se crearan con el tamaño especificado
+	en porcentaje_test.
+
+	"""
 	if predictores_test == None or etiquetas_test == None:
 		# separamos en entrenamiento y test dejando un 80% de los datos en entrenamiento
 		# y un 20% en test
@@ -44,7 +67,7 @@ def entrenar_modelo(modelo, predictores, etiquetas, predictores_test = None, eti
 	test_accuraccy = np.mean(modelo.predict(predictores_test) == etiquetas_test)
 
 
-	return train_accuraccy, test_accuraccy
+	return modelo, train_accuraccy, test_accuraccy
 
 
 
@@ -67,34 +90,33 @@ def main():
 	print("Tamaño de las etiquetas: ", etiquetas.shape)
 
 	print("Pasamos a probar un modelo básico de regresión logistica.")
-	modelo_RL = skl.linear_model.LogisticRegression()
 
-	train_accuraccy_RL, test_accuraccy_RL = entrenar_modelo(modelo_RL, predictores, etiquetas)
+	modelo_RL, train_accuraccy_RL, test_accuraccy_RL = entrenar_modelo(skl.linear_model.LogisticRegression(), predictores, etiquetas)
 
 	print("Accuraccy en train con regresión logistica: ", train_accuraccy_RL)
 	print("Accuraccy en test con regresión logistica: ", test_accuraccy_RL)
 	print()
 
 
-	train_accuraccy_tree, test_accuraccy_tree = entrenar_modelo(skl.tree.DecisionTreeClassifier(), predictores, etiquetas)
+	modelo_DT, train_accuraccy_tree, test_accuraccy_tree = entrenar_modelo(skl.tree.DecisionTreeClassifier(), predictores, etiquetas)
 
 	print("Accuraccy en train con un árbol de decisión: ", train_accuraccy_tree)
 	print("Accuraccy en test con un árbol de decisión: ", test_accuraccy_tree)
 	print()
 
-	train_accuraccy_RF, test_accuraccy_RF = entrenar_modelo(skl.ensemble.RandomForestClassifier(), predictores, etiquetas)
+	modelo_RF, train_accuraccy_RF, test_accuraccy_RF = entrenar_modelo(skl.ensemble.RandomForestClassifier(), predictores, etiquetas)
 
 	print("Accuraccy en train con Random Forest: ", train_accuraccy_RF)
 	print("Accuraccy en test con Random Forest: ", test_accuraccy_RF)
 	print()
 
-	train_accuraccy_svc, test_accuraccy_svc = entrenar_modelo(skl.svm.SVC(), predictores, etiquetas)
+	modelo_SVC, train_accuraccy_svc, test_accuraccy_svc = entrenar_modelo(skl.svm.SVC(), predictores, etiquetas)
 
 	print("Accuraccy en train con SVC: ", train_accuraccy_svc)
 	print("Accuraccy en test con SVC: ", test_accuraccy_svc)
 	print()
 
-	train_accuraccy_MLP, test_accuraccy_MLP = entrenar_modelo(skl.neural_network.MLPClassifier(), predictores, etiquetas)
+	modelo_MLP, train_accuraccy_MLP, test_accuraccy_MLP = entrenar_modelo(skl.neural_network.MLPClassifier(), predictores, etiquetas)
 
 	print("Accuraccy en train con MLP: ", train_accuraccy_MLP)
 	print("Accuraccy en test con MLP: ", test_accuraccy_MLP)
